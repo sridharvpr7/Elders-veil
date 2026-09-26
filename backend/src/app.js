568215a38baa9fce1a07c49cf0190035de8d9b8e
Sridhar Vpr <sridharvpr7@gmail.com>
2026-09-26 14:54:20 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,9 +16,6 @@ const errorMiddleware = require('./middleware/errorMiddleware');
 
 const app = express();
 
-// Render reverse proxy support
-app.set('trust proxy', 1);
-
 // Security Headers with relaxed directive for image serving and canvas
 app.use(helmet({
   contentSecurityPolicy: false,
